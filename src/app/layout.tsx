1969e08fb13a4ecc9e9bcdb0f09669fc96abf143
Rodrigo Midões <rodrigo.midoes@bravantic.com>
2025-08-28 11:16:50 +0100

    Implement Google Tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-2KCB8B059C"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2KCB8B059C');
+          `}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
       >
